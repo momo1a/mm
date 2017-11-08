9b36acb680a836cf0161f03708dc4dea15d2b9f1
moshiyou-home <momo1a@qq.com>
2017-11-08 22:59:54 +0800

    【修改】配置项

--- src/common/config/production/mm.php
+++ src/common/config/production/mm.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['domain_www'] = 'http://www.h3hq.com';
-$config['domain_img'] = 'http://img.h3hq.com';
-$config['domain_static'] = 'http://static.h3hq.com';
-$config['domain_detail'] = 'http://detail.h3hq.com';
-$config['domain_list'] = 'http://list.h3hq.com';
+$config['domain_www'] = 'http://www.test.com';
+$config['domain_img'] = 'http://img.test.com';
+$config['domain_static'] = 'http://static.test.com';
+$config['domain_detail'] = 'http://detail.test.com';
+$config['domain_list'] = 'http://list.test.com';
